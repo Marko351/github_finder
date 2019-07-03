71abc8c9c0a0e7c15d28dcdb804bb9b4f1174ca7
Marko351 <markodjuric351@gmail.com>
2019-07-03 18:59:11 +0200

    Created Users and UserItem components with some aditional configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className='navbar bg-primary'>
-        <h1>
-          <i className='fab fa-github' />
-          Github Finder
-        </h1>
-      </div>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className='fab fa-github' />
+        Github Finder
+      </h1>
+    </div>
+  );
+};
 
 export default Navbar;
