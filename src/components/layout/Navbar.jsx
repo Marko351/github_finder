5d30b7ade639b89e632fdaccef3a39865409619a
Marko351 <markodjuric351@gmail.com>
2019-07-03 17:43:52 +0200

    Initial commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className='navbar bg-primary'>
+        <h1>
+          <i className='fab fa-github' />
+          Github Finder
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
